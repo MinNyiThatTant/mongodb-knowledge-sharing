7402999e2cc34d4bb72e0d3177b772d28b54e43d
MinNyiThatTant <minnyithattant@gmail.com>
2023-07-11 23:57:53 +0900

    s

--- 5.CRUD- with nodejs/server.js
+++ 5.CRUD- with nodejs/server.js
@@ -1,44 +1,17 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app=express()
-app.use(express.json())
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
 
-mongoose.set("strictQuery",false)
-mongoose.connect("mongodb://127.0.0.1:27017/univdb", {
-},(err)=>{
+mongoose.connect("mongodb://localhost:27017/mydb",{
+
+},(err)=> {
     if(!err){
-        console.log('good db connect')
+        console.log("|connect mongodb")
     }else{
         console.log(err)
     }
 })
 
-//schema
-const schema = {
-    name: String,
-    email: String, 
-    id: Number
-}
-
-const dbmodel = mongoose.model("dept", schema)
-
-//post
-app.post('/some', async(req,res)=>{
-    console.log('some post')
-    
-    const data = new dbmodel({
-        name: req.body.name,
-        email: req.body.email,
-        id: req.body.id
-    })
-
-    const a  = await data.save()
-    res.json(a)
-})
-
-
-
-
 app.listen(3000, ()=> {
-    console.log('3000')
+    console.log("port 3000")
 })
