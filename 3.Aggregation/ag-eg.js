ca9c282773921052daa6978121800e298e0ac179
MinNyiThatTant <minnyithattant@gmail.com>
2023-07-11 17:27:39 +0900

    something

--- 3.Aggregation/ag-eg.js
+++ 3.Aggregation/ag-eg.js
@@ -1,25 +1,34 @@
 ([
     {
-    "regno": "121",
-    "name": "koko",
-    "category": {
-        "myanmar": 50, "english": 50, "maths": 90
-    }
-},
-
-{
-    "regno": "120",
-    "name": "mama",
-    "category": {
-        "myanmar": 100, "english": 30, "maths": 80
-    }
-},
-
-{
-    "regno": "123",
     "name": "mgmg",
-    "category": {
-        "myanmar": 75, "english": 85, "maths": 50
-    }
-}
+    "address": "kyaukse",
+    "salary": 300000
+},
+
+{
+    "name": "koko",
+    "address": "mandalay",
+    "salary": 500000
+},
+
+{
+    "name": "minmin",
+    "address": "monywa",
+    "salary": 800000
+},
+{ 
+    "name": "mgmg",
+    "address": "pathein",
+    "salary": 700000
+},
+{ 
+    "name": "mama",
+    "address": "yangon",
+    "salary": 900000
+},
+{ 
+    "name": "tuntun",
+    "address": "shwebo",
+    "salary": 500000
+},
 ])
---
+++ 3.Aggregation/ag.js
@@ -0,0 +1,50 @@
+db.employee.aggregate({$match:{address:"kyaukse"}})
+
+db.employee.aggregate({$match:{name:{$eq:"mandalay"}}})
+
+db.employee.aggregate({$match:{$and:[{salary:{$eq:300000}},{name:{$eq:"koko"}}]}})
+
+db.employee.aggregate({$project:{name:1, salary:1,_id:0}})
+
+db.employee.aggregate({$project:{address:1,_id:0}})
+
+//match and project
+db.employee.aggregate({$match:{salary:{$gt:10000}}},
+                      {$project:{name:1,salary:1}}
+    )
+
+
+//group
+db.employee.find({},{address:1,salary:1,_id:0})
+
+
+// {$group: {_id:<expression>, expression <field1>:{<accumulator1>:<expression1>}, ... }}
+
+// Accumulator Operator 
+$avg
+$first
+$last
+$max
+$min
+$sum
+
+
+db.employee.aggregate({$group:{_id:"$address", total_salary:{$sum:"$salary"}}})
+
+// $out <= copy collection
+db.employee.aggregate({$out:"employee1"})
+
+// $match, $group, $out
+db.employee.aggregate({$match:{address:"mandalay"}},
+                    {$group:{_id:"$address", total_count:{$sum:1}}}                      
+                    {$out:"next_employee"}
+)
+
+db.next_employee.find()
+
+db.employee.aggregate(
+                    {$match:{address:"kyaukse"}},
+                    {$count:"total_count"},
+                    {$out:"another_employee"}
+)
+
