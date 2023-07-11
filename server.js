602e27107788317852a6d167b2c72280a314b9d6
MinNyiThatTant <minnyithattant@gmail.com>
2023-07-11 10:46:33 +0900

    some

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const app=express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('hell browser')
+mongoose.set("strictQuery",false)
+mongoose.connect("mongodb://127.0.0.1:27017/univdb", {
+},(err)=>{
+    if(!err){
+        console.log('good db connect')
+    }else{
+        console.log(err)
+    }
 })
 
-app.listen(3000, () => {
-    console.log('hello port 3000')
+//schema
+const schema = {
+    name: String,
+    email: String, 
+    id: Number
+}
+
+const dbmodel = mongoose.model("dept", schema)
+
+//post
+app.post('/some', async(req,res)=>{
+    console.log('some post')
+    
+    const data = new dbmodel({
+        name: req.body.name,
+        email: req.body.email,
+        id: req.body.id
+    })
+
+    const a  = await data.save()
+    res.json(a)
+})
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('3000')
 })
